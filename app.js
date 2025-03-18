9b5a8719a3d028142799355887ead4c55c76b2ab
Silvia <sindelsevilla10@gmail.com>
2025-03-18 14:17:29 -0600

    Logica desafio amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+    
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+    
+    amigos.push(nombre);
+    input.value = "";
+    mostrarLista();
+}
+
+function mostrarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    amigos.forEach((nombre) => {
+        const li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+    
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+    
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li> ${amigoSorteado} es el amigo seleccionado! </li>`;
+}
